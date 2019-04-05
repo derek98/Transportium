710bb47e339eb5903e70fb562ce8e4c4c92732f5
Ferran Escolano <alu2014044@a22o12.local>
2019-04-05 17:48:18 +0200

    Changing screen when Logging in

---
+++ Transportium/Login/CreateProfile.swift
@@ -0,0 +1,17 @@
+
+
+import UIKit
+
+class CreateProfile: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
--- Transportium/Model/Log In.swift
+++ Transportium/Model/Log In.swift
@@ -21,18 +21,36 @@ class Log_In: UIViewController {
     
    
     @IBAction func Log(_ sender: Any) {
-        print("TUPUTAMADRE")
+        
         let emailLog = self.email.text
         let passwordLog = self.password.text
         
         
+       
+        let newViewController = storyboard?.instantiateViewController(withIdentifier: "createProfile") as! CreateProfile
+        
+        
         Auth.auth().signIn(withEmail: emailLog!, password: passwordLog!) { (user, error) in
+            
+            if ((emailLog?.isEmpty)! || (passwordLog?.isEmpty)!){
+                let alert = UIAlertController(title: "Error", message: "Hay campos vacíos", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Aceptar", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
+            }
             
             if let error = error {
                 print(error.localizedDescription)
+                let alert = UIAlertController(title: "Error", message: "Usuario o contraseña incorrecto", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Aceptar", style: .cancel, handler: nil))
+                self.present(alert, animated: true)
             }
             else if let user = user {
+                
+                
+                
                 print("Log Successfully.")
+                self.present(newViewController, animated: true, completion: nil)
+            
             }
         }
 
