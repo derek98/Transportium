c4935fdd1105c7b1fa7cf69f684f8d5521b2c17b
Ferran Escolano <alu2014044@a22o12.local>
2019-04-05 16:56:07 +0200

    as

--- Transportium/Model/Log In.swift
+++ Transportium/Model/Log In.swift
@@ -4,8 +4,10 @@ import FirebaseUI
 
 class Log_In: UIViewController {
     
+    
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
+    
     
     
     
@@ -21,6 +23,38 @@ class Log_In: UIViewController {
     
     @IBAction func Login(_ sender: Any) {
         
+      /*  let correo = email.text
+        let contraseña = password.text
+        
+        
+        if correo == "" || contraseña == "" {
+            let alertController = UIAlertController(title: "Error", message: "Por favor introduce email y contraseña", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            //2.
+            Auth.auth().signIn(withEmail: correo!, password: contraseña!) { (user, error) in
+                //3.
+                if error == nil {
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    //4.
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+        */
         let correo = email.text
         let contraseña = password.text
         
@@ -29,12 +63,14 @@ class Log_In: UIViewController {
             
             
             if let _eror = error{
+                print("Login incorrecto")
                 print(_eror)
                 
             }else{
                 
                 
                 if let _res = result{
+                    print("Login correcto")
                     print(_res)
                 }
             }
